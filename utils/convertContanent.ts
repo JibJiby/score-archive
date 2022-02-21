d7e520ffd5b8e852c03cf4a2c1d5cfe52d48a38d
jibjiby <my01sun@gmail.com>
2022-02-21 22:45:56 +0900

    feat: 자음 분리 util 함수 추가

---
+++ utils/convertContanent.ts
@@ -0,0 +1,14 @@
+import * as Hangul from 'hangul-js'
+
+/**
+ * 문자열을 자음으로 변환하는 함수
+ */
+const convertConsonant = (word: string): string => {
+  // 2번째 인자 true인 경우 한글자당 그룹으로 묶어줌.
+  const d = Hangul.disassemble(word.replace(/\s/, ''), true)
+  const result: string[] = []
+  d.forEach((v) => result.push(v[0]))
+  return result.join('')
+}
+
+export default convertConsonant
