795e955c4e22cbc81e33689c7191dc6b3278398a
jibjiby <my01sun@gmail.com>
2022-02-22 02:15:07 +0900

    chore: 코멘트 제거

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import AppLayout from '@components/AppLayout'
 import ScoreSearch from '@components/ScoreSearch'
-import { useSelector } from 'react-redux'
-import { RootState } from '@reducers/index'
 import ScoreResultList from '@components/ScoreResultList'
 
 const Home = () => {
