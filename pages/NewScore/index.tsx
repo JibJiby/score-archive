6f2f3460a5ac7ac63aff89591e1851b43c7c4761
jibjiby <my01sun@gmail.com>
2022-02-22 15:28:10 +0900

    feat: 업로드 UI 추가

--- pages/NewScore/index.tsx
+++ pages/NewScore/index.tsx
@@ -1,26 +1,33 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import AppLayout from '@components/AppLayout'
 import useInput from '@hooks/useInput'
-import { inputStyle } from '@styles/common'
 import convertConsonant from '@utils/convertContanent'
-import { message } from 'antd'
+import { Image, message } from 'antd'
 import { useUploadFile } from 'react-firebase-hooks/storage'
 import { firestore, storage } from '../../firebase'
 import { ref } from 'firebase/storage'
 import { addDoc, collection } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
 import { QueryResult } from '@reducers/score'
+import { FileUploadWrapper, NewScoreInput } from './styles'
 
 const NewScore = () => {
   const [newScoreTitle, onChangeScoreTitle] = useInput('')
   const [fileType, setFileType] = useState('')
   const [selectedFile, setSelectedFile] = useState<File>()
+  const [localFileUrl, setLocalFileUrl] = useState('')
+  const uploadRef = useRef<HTMLInputElement>(null)
 
   // TODO: uploading 스피너 적용
   const [uploadFile] = useUploadFile()
   const navigate = useNavigate()
 
   const onUpload = useCallback(async () => {
+    if (newScoreTitle === '') {
+      message.warn('제목을 먼저 입력해주세요.')
+      return
+    }
+
     if (selectedFile) {
       // storage
       const encodedName = encodeURI(newScoreTitle)
@@ -56,7 +63,8 @@ const NewScore = () => {
           width: '100%',
           height: '100%',
           display: 'flex',
-          justifyContent: 'center',
+          marginTop: '4.5rem',
+          // justifyContent: 'center',
           flexDirection: 'column',
           alignContent: 'center',
         }}
@@ -67,33 +75,81 @@ const NewScore = () => {
             justifyContent: 'center',
           }}
         >
-          <label>제목</label>
-          <input value={newScoreTitle} onChange={onChangeScoreTitle} css={inputStyle} />
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <input
-            type="file"
-            onChange={(e) => {
-              const file = e.target.files ? e.target.files[0] : undefined
-              if (file) {
-                setFileType(file.type)
-                setSelectedFile(file)
-              }
-            }}
+          <NewScoreInput
+            value={newScoreTitle}
+            onChange={onChangeScoreTitle}
+            placeholder={'악보 제목을 입력해주세요.'}
           />
         </div>
         <div
           style={{
             display: 'flex',
             justifyContent: 'center',
+            padding: '25px',
           }}
         >
-          <button onClick={onUpload}>추가</button>
+          {selectedFile ? (
+            <Image src={localFileUrl} />
+          ) : (
+            <FileUploadWrapper
+              onClick={() => {
+                if (uploadRef.current) uploadRef.current.click()
+              }}
+            >
+              <span role="button" tabIndex={0}>
+                <input
+                  type="file"
+                  accept="image/jpg,impge/png,image/jpeg,image/gif"
+                  onChange={(e) => {
+                    const file = e.target.files ? e.target.files[0] : undefined
+                    if (file) {
+                      //https://gaemi606.tistory.com/39
+                      let reader = new FileReader()
+                      reader.readAsDataURL(file)
+                      reader.onloadend = () => {
+                        setLocalFileUrl(reader.result as string)
+                      }
+
+                      setFileType(file.type)
+                      setSelectedFile(file)
+                    }
+                  }}
+                  ref={uploadRef}
+                  style={{ display: 'none' }}
+                />
+                <div>
+                  <div style={{ marginTop: '8px', fontWeight: 700 }}>업로드</div>
+                </div>
+              </span>
+            </FileUploadWrapper>
+          )}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <button
+            onClick={onUpload}
+            // TODO: ScoreSearch 스타일 묶기
+            style={{
+              width: '200px',
+              height: '2.5rem',
+              margin: '50px auto',
+              border: 0,
+              borderRadius: '6px',
+              fontSize: '20px',
+              backgroundColor: '#001C34',
+              color: 'white',
+              fontWeight: 'bold',
+              userSelect: 'none',
+              cursor: 'pointer',
+              transition: 'all 0.5s',
+            }}
+          >
+            추가
+          </button>
         </div>
       </div>
     </AppLayout>
---
+++ pages/NewScore/styles.tsx
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import { inputStyle } from '@styles/common'
+
+export const NewScoreInput = styled.input`
+  ${inputStyle}
+
+  /* TODO: ScoreSearch 스타일 일반화 */
+  margin: 50px auto;
+  width: 60%;
+  height: 3rem;
+  outline: none;
+  border: 0px;
+  font-size: 16px;
+  padding: 10px;
+  border-radius: 6px;
+  text-align: center;
+`
+
+export const FileUploadWrapper = styled.div`
+  width: 128px;
+  height: 128px;
+  background-color: #fafafa;
+  cursor: pointer;
+  border: 1.4px dashed #d9d9d9;
+
+  &:hover {
+    border-color: tomato;
+  }
+
+  span[role='button'] {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+`
