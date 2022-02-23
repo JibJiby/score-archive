5fe824d364ff37bdc390462113d61c18f57b198f
jibjiby <my01sun@gmail.com>
2022-02-23 13:42:02 +0900

    feat: 새로고침시 상단 자동 이동 추가

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 
 const AppLayout: React.FC = ({ children }) => {
+  useEffect(() => {
+    // 새로고침 시 상단으로 화면 올리기
+    // react-beforeunload 라이브러리도 있음
+    // FIXME: 스마트폰은 안됨
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0)
+    }
+  }, [])
+
   return (
     <>
       <Header />
